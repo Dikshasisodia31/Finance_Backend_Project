faaf95692f1896fc696f90e8a9200f7afb269107
Dikshasisodia31 <dikshasisodia3112@gmail.com>
2026-04-02 22:59:04 +0530

    Add record routes

--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    createRecord,updateRecord,deleteRecord
+    createRecord,updateRecord,deleteRecord,
+    getRecords
 } = require("../controllers/recordController");
 
 const authMiddleware = require("../middlewares/authmiddleware");
 
 router.post("/",authMiddleware,createRecord);
+router.get("/",authMiddleware,getRecords);
 router.put("/:id",authMiddleware,updateRecord);
 router.delete("/:id",authMiddleware,deleteRecord);
 
